ce7272c5b7b9c53b40ff5c1b116aff8ec5ce4c60
adi-23 <vallinarasimhaswamy.k19@iiits.in>
2022-01-02 17:11:31 +0530

    Resolved Errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Routes, Route} from "react-router-dom";
-import { Admin, Resource } from 'react-admin'
-import restProvider from 'ra-data-simple-rest'
-import CategoryList from './components/admin/CategoryList'
-import CategoryCreate from './components/admin/CategoryCreate'
-import CategoryEdit from './components/admin/CategoryEdit';
-import UserList from './components/admin/UserList'
-import UserCreate from './components/admin/UserCreate'
-import UserEdit from './components/admin/UserEdit';
-import BrandList from './components/admin/BrandList';
-import BrandCreate from './components/admin/BrandCreate';
-import BrandEdit from './components/admin/BrandEdit';
-import SellerList from './components/admin/SellerList';
-import SellerCreate from './components/admin/SellerCreate';
-import SellerEdit from './components/admin/SellerEdit';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+// import { Admin, Resource } from 'react-admin'
+// import restProvider from 'ra-data-simple-rest'
+// import CategoryList from './components/admin/CategoryList'
+// import CategoryCreate from './components/admin/CategoryCreate'
+// import CategoryEdit from './components/admin/CategoryEdit';
+// import UserList from './components/admin/UserList'
+// import UserCreate from './components/admin/UserCreate'
+// import UserEdit from './components/admin/UserEdit';
+// import BrandList from './components/admin/BrandList';
+// import BrandCreate from './components/admin/BrandCreate';
+// import BrandEdit from './components/admin/BrandEdit';
+// import SellerList from './components/admin/SellerList';
+// import SellerCreate from './components/admin/SellerCreate';
+// import SellerEdit from './components/admin/SellerEdit';
 import SellerBrandList from './components/seller/SellerBrandList'
+import SellerLogin from './components/seller/SellerLogin';
 function App() {
   return (
     // <Admin dataProvider={restProvider('http://localhost:3000')}>
@@ -43,7 +45,7 @@ function App() {
     //     edit={SellerEdit}
     //   />
     // </Admin>
-    
+    <Router>
       <div className='App'>
 
       <Routes>
@@ -52,7 +54,7 @@ function App() {
 
 
       </div>
-
+    </Router>
   )
 
   
--- client/src/components/seller/SellerBrandList.js
+++ client/src/components/seller/SellerBrandList.js
@@ -1,26 +1,9 @@
 import React from 'react'
-import axios from 'axios';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Container from '@mui/material/Container'
-import { Card, Grid } from '@material-ui/core';
-export default function SellerBrandList() {
-    const [brands,setBrands]=useState([]);
-    useEffect(() => {
-        const brandinfo=[]
-        axios.get('https://localhost:5000/brands',brandinfo)
-        setBrands(brandinfo)
-    })
 
-    const {b} = brands 
+export default function SellerBrandList() {
     return (
         <div>
-            {b.map((brand) => <Container maxWidth="xs">
-              <Card raised="true">
-                  brand.name
-              </Card>
-
-            </Container>
-            )}
+            hi
         </div>
     )
 }
