a78368cd3b36c6c0f2561377783d28104d300887
varshini123456 <kvsvarshini23@gmail.com>
2022-01-11 16:17:13 +0530

    changes in the product

--- client/src/components/user/UserHomepage/Product.js
+++ client/src/components/user/UserHomepage/Product.js
@@ -1,23 +1,93 @@
 import React from "react";
-import {NavLink} from "react-router-dom"
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+import { mergeClasses } from "@material-ui/styles";
+import useStyles from "../Shopping/Styles";
+import sellerstore from "../../seller/sellerstore";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import {connect} from "react-redux"
 
-function Product(props){
+// displaying the product with its features
+
+const Product = ({product,username,cart}) => {
+    const classes =useStyles();
+
+    const addToCart = (product,username,cart) =>{  // Add to cart functionality
+        console.log(username)
+        let flag = 0
+        for(let c of cart){
+            if(c.id === product.id){
+                if(c.qty < product.productquantity){    // checking if stock of product is available
+                    flag =1
+                    sellerstore.dispatch({type : "ADD_TO_CART", payload : {item : product, username : username }})
+                }
+                else{
+                    flag = 1
+                    alert('No stock left to order') // No stock left
+                }
+            }
+            
+        }
+        if(flag === 0){
+            
+                sellerstore.dispatch({type : "ADD_TO_CART", payload : {item : product, username : username }})
+        }
+    }
+
+
     return (
-        // displaying single product
-        <>
-            <div className="single-product col">
-                <div className="product-f-image">
-                    <img src="" alt="" />      
-                </div>
-                                            
-                <h2><NavLink to="single-product.html">Samsung Galaxy s5- 2015</NavLink></h2>
-                                            
-                <div className="product-carousel-price">
-                    <ins>$700.00</ins> <del>$100.00</del>
-                </div> 
-            </div>
-        </>
-    );
-}
+        <Card className ={classes.root} style={{width : 300}}>
+            <CardMedia className ={classes.media} image="https://images.samsung.com/is/image/samsung/assets/in/smartphones/mobiles-by-camera/sm-m022gzadins.png?$720_N(384)_JPG$" title={Product.productname} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.productname}
 
-export default Product;
+                    </Typography>
+                    <Typography variant ="h5">
+                        {product.productprice}/-
+
+                    </Typography>
+
+                </div>
+                <Typography variant ="body2" color="textSecondary">
+                    {/* {product.productbrand} brand,
+                    {product.ram} ram,
+                    {product.colour},
+                    {product.storage} storage */}
+
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+            <div className='mt-3'>
+           
+            <button onClick={(e) => addToCart(product,username,cart)}
+                    className="btn cart-btn">Add To Cart</button>
+                    
+   
+            <IconButton aria-label="Add to Cart"  />
+                    
+            </div>
+
+            <AddShoppingCart/>
+            </CardActions>
+
+            
+
+        </Card>
+        
+        
+
+    );
+
+    };
+
+
+    const mapStateToProps = (state) => {
+        return {
+          username: state.userReducer.username,
+          cart : state.cartReducer.cart
+        };
+      };
+export default connect(mapStateToProps)(Product);
--- client/src/components/user/UserHomepage/Products.js
+++ client/src/components/user/UserHomepage/Products.js
@@ -1,98 +1,75 @@
-import React from "react";
-import img1 from "./Images/product-1.jpg"
-import img2 from "./Images/product-2.jpg"
-import img3 from "./Images/product-3.jpg"
-import img4 from "./Images/product-4.jpg"
-import img5 from "./Images/product-5.jpg"
-import img6 from "./Images/product-6.jpg"
-import {NavLink} from "react-router-dom"
-import "./CSS/responsive.css"
+import React from "react"
+import {Grid} from '@material-ui/core';
+import Product from "./Product";
+import useStyles from "../Shopping/Styles";
+import Header from '../UserHomepage/Header';
+import {useState, useEffect} from 'react'
+import { useLocation } from "react-router-dom";
+import Autocomplete from '@mui/material/Autocomplete';
+import { FormControl, TextField, Typography } from '@mui/material';
+import { Button } from '@mui/material';
+import sellerstore from '../../seller/sellerstore';
+import {connect} from 'react-redux'
 
 
-// displaying products
-function Products(){
+function Products({username}){
+
+    const classes = useStyles();
+
+    const [productinfo,setProductinfo] = useState({
+        products: []
+    })
+
+
+    const fetchdata = ()=>{
+
+
+            return fetch(`http://localhost:5000/sellerproduct`).then((response)=>
+         response.json()).then((data)=>{
+             console.log(data)
+             setProductinfo({
+             products: data
+         })
+        
+         
+     })
+        
+
+    
+
+        
+     }
+
+    useEffect(() => {
+        fetchdata()
+        
+    },[])
+    const {products} = productinfo
+
     return (
-        <>
-            
-            <div className="maincontent-area">
-                <div className="zigzag-bottom"></div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="latest-product">
-                                <h2 className="section-title">Latest Products</h2>
-                                <div className="container">
-                                    <div className="product-carousel row">
-                                        <div className="single-product col">
-                                            <div className="product-f-image">
-                                                <img src={img1} alt="" />
-                                                
-                                            </div>
-                                            
-                                            <h2><NavLink to="single-product.html">Samsung Galaxy s5- 2015</NavLink></h2>
-                                            
-                                            <div className="product-carousel-price">
-                                                <ins>$700.00</ins> <del>$100.00</del>
-                                            </div> 
-                                        </div>
-                                        <div className="single-product col">
-                                            <div className="product-f-image">
-                                                <img src={img2} alt="" />
-                                                
-                                            </div>
-                                            
-                                            <h2><NavLink to="single-product.html">Nokia Lumia 1320</NavLink></h2>
-                                            <div className="product-carousel-price">
-                                                <ins>$899.00</ins> <del>$999.00</del>
-                                            </div> 
-                                        </div>
-                                        <div className="single-product col">
-                                            <div className="product-f-image">
-                                                <img src={img3} alt="" />
-                                                
-                                            </div>
-                                            
-                                            <h2><NavLink to="single-product.html">LG Leon 2015</NavLink></h2>
-
-                                            <div className="product-carousel-price">
-                                                <ins>$400.00</ins> <del>$425.00</del>
-                                            </div>                                 
-                                        </div>
-                                        <div className="single-product col">
-                                            <div className="product-f-image">
-                                                <img src={img4} alt="" />
-                                                
-                                            </div>
-                                            
-                                            <h2><NavLink to="single-product.html">Sony microsoft</NavLink></h2>
-
-                                            <div className="product-carousel-price">
-                                                <ins>$200.00</ins> <del>$225.00</del>
-                                            </div>                            
-                                        </div>
-                                        <br />
-                                        <div className="single-product col">
-                                            <div className="product-f-image">
-                                                <img src={img5} alt="" />
-                                                
-                                            </div>
-                                            
-                                            <h2><NavLink to="single-product.html">iPhone 6</NavLink></h2>
-
-                                            <div className="product-carousel-price">
-                                                <ins>$1200.00</ins> <del>$1355.00</del>
-                                            </div>                                 
-                                        </div>
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+        <div>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product)=>{
+                    
+                    return (
+                        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                    );
+                    
+                })}
+                </Grid> 
+        </div>
     );
 }
 
-export default Products;
+const mapStateToProps= (state)=>{
+    console.log(state.brandReducer.brand)
+    
+    return {
+        brandname: state.brandReducer.brand,
+        username: state.userReducer.username,
+
+    }
+}
+export default connect(mapStateToProps)(Products)
